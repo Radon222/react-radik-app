cc5013155381dfa0301e0538386e15f80724d0eb
Radon222 <89600373712@mail.ru>
2021-10-13 13:12:24 +0300

    css module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className='header'>
+    <header className={classes.header}>
       <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeEA9kk11Kv2RUzcmUxlTzHIVY7S5mdA0PFA&usqp=CAU' />
     </header>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className='content'>
+    <div className={classes.content}>
       <div>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKCPDuNaukD8ipmkQrrl0b0Lk-I2k-_Ogfmg&usqp=CAU' />
       </div>
-      <div>avaAA + description</div>
+      <div>ava + description</div>
       <div>
         My Posts
-        <div>New Post</div>
+        <div className={classes.posts}>New Post</div>
         <div>
-          <div>post1</div>
-          <div>post2</div>
+          <div className={classes.item}>post1</div>
+          <div className={classes.item}>post2</div>
         </div>
       </div>
     </div>
