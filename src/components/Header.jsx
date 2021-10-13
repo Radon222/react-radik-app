2c0f8b686320cde0852ab4bcdb9ea055743d91b6
Radon222 <89600373712@mail.ru>
2021-10-13 10:38:19 +0300

    Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeEA9kk11Kv2RUzcmUxlTzHIVY7S5mdA0PFA&usqp=CAU' />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKCPDuNaukD8ipmkQrrl0b0Lk-I2k-_Ogfmg&usqp=CAU' />
+      </div>
+      <div>avaAA + description</div>
+      <div>
+        My Posts
+        <div>New Post</div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
