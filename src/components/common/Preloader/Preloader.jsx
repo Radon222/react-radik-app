f0b8911852252d615f1935639dda2c34f43a5ba0
Radon222 <89600373712@mail.ru>
2021-10-25 11:38:31 +0300

    preloader

---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,10 @@
+import preloader from '../../../assets/imeges/preloader.svg'
+import React from 'react'
+
+const Preloader = () => {
+    return (
+        <img alt='123' src={preloader} />
+    )
+}
+
+export default Preloader;
